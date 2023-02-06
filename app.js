381f9aec6dbd580ebb14e6d2bb20c5a0e84d3764
ajith vignesh <vigneshajith152@gmail.com>
2023-02-06 14:30:49 +0530

    every thing is back to normal pls dont revert back from this commit

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const mongoose = require('mongoose')
 const encrypt  = require("mongoose-encryption")
 
 // const mongodbPass = process.env.MONGODBPASS;
-const secrets = "This a secret" ;
 
 // mongoose.connect(
 //   "mongodb+srv://vignesh-admin:"+mongodbPass+"@cluster23.2fymgyj.mongodb.net/"
@@ -16,16 +15,20 @@ const secrets = "This a secret" ;
 mongoose.set("strictQuery", false);
 mongoose.connect('mongodb://127.0.0.1:27017/userdb')
 
-const userSchema = mongoose.Schema({
-    email: String,
-    password:String
+const userSchema = new mongoose.Schema({
+  email: String,
+  password:String
 })
 
-
-const UserId = mongoose.model("UserId", userSchema);
-
+const secrets = "This a secret" ;
 
 userSchema.plugin(encrypt, { secret: secrets, encryptedFields: ["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+User.find({}, (err, user) => {
+  console.log(user);
+})
 
 
 app.use(express.static('public'));
@@ -37,13 +40,44 @@ app.get("/", (req, res) => {
     res.render("home")
 });
 
-app.get("/register", (req, res) => {
+app.route("/register").get( (req, res) => {
   res.render("register");
-});
+}).post((req, res) => {
 
-app.get("/login", (req, res) => {
+  newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save((err, result) => {
+    if (result) {
+      res.render("secrets")
+    } else {
+      res.render(err)
+    }
+  })
+
+})
+
+app.route("/login").get((req, res) => {
   res.render("login");
-});
+}).post((req, res) => {
+  const username = req.body.username;
+  const passWord = req.body.password;
+  console.log(username);
+  User.findOne({ email: req.body.username }, (err, user) => {
+    console.log(user);
+    if (user) {
+      if (user.password === passWord) {
+        res.render("secrets");
+      } else {
+        res.send("password is incorrect");
+      }
+    } else {
+      res.send("email is not fount");
+    }
+  });
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
