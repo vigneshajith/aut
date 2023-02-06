245588abfff5dbb4590c7351fc5f7fc9ad7a1331
ajith vignesh <vigneshajith152@gmail.com>
2023-02-06 11:16:54 +0530

    Revert "mongoos-encryption npm install rollback from encryption failed"
    This reverts commit 49be90564e2dcc2999729bfd6edd0be29c2ea1ea.

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-require('dotenv').config()
-const express = require("express");
+const express = require('express');
 const app = express();
 
-require("ejs");
+require('ejs')
 
-const { urlencoded } = require("body-parser");
-const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const {urlencoded} = require('body-parser');
+const mongoose  = require('mongoose')
 
 // const mongodbPass = process.env.MONGODBPASS;
 const secrets = "This a secret" ;
@@ -15,75 +13,38 @@ const secrets = "This a secret" ;
 //   "mongodb+srv://vignesh-admin:"+mongodbPass+"@cluster23.2fymgyj.mongodb.net/"
 // );
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/userdb");
+mongoose.connect('mongodb://127.0.0.1:27017/userdb')
 
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-});
+const userSchema = mongoose.Schema({
+    email: String,
+    password:String
+})
+
+
+const UserId = mongoose.model("UserId", userSchema);
 
 
 userSchema.plugin(encrypt, { secret: secrets, encryptedFields: ["password"] });
 
-const User = mongoose.model("User", userSchema);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(urlencoded({ extended: true }));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  res.render("home");
+    res.render("home")
 });
 
-app
-  .route("/register")
-  .get((req, res) => {
-    res.render("register");
-  })
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-  .post((req, res) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: req.body.password,
-    });
-    newUser.save((err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
-
-app
-  .route("/login")
-  .get((req, res) => {
-    res.render("login");
-  })
-  .post((req, res) => {
-    const username = req.body.username;
-    const passWord = req.body.password;
-    User.findOne({ email: username}, (err, user) => {
-     if(err){
-      res.send(err)
-     }else{
-      if(user){
-            if(user.email===username && user.password === passWord ){
-              res.render("secrets");
-              console.log(user.email,user.password);
-            }else{
-              res.send("your password is wrong")
-            }
-          }else{
-        res.send("your Email or password is incorrect")
-      }
-     }
-    }
-    );
-  });
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("serve is running on port " + port);
-});
+    console.log("serve is running on port "+port);
+})
